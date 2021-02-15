7c71cf381e69dab6fd05cb4aae8b3e6d44261c0a
masuyama13 <masterxwebm13@gmail.com>
2021-02-15 12:01:46 +0900

    バリデーションを追加

--- app/models/borrowing_thing.rb
+++ app/models/borrowing_thing.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class BorrowingThing < ApplicationRecord
+  validates :name, presence: true # TODO: lengthを設定する
+  validates :borrow_from, presence: true # TODO: lengthを設定する
+  validate :remind_on_should_be_after_borrowed_on
+
+  private
+    def remind_on_should_be_after_borrowed_on
+      return unless remind_on && borrowed_on
+
+      if remind_on <= borrowed_on
+        errors.add(:remind_on, "は貸借日よりも後に設定してください")
+      end
+    end
 end
