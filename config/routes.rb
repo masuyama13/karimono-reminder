d5fd0980af5822d27dbbe908a48d5560b56a31c5
masuyama13 <masterxwebm13@gmail.com>
2021-02-19 15:44:18 +0900

    Usersコントローラ・ビュー・ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   root to: "borrowing_things#index"
+  resources :users, except: [:index]
   resources :borrowing_things
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
