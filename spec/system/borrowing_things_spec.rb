3b43eae39daecde26e0b9eeca99aa1081a1a0b75
masuyama13 <masterxwebm13@gmail.com>
2021-02-22 16:31:28 +0900

    かりもののシステムスペックでログインヘルパーを使うようにした

--- spec/system/borrowing_things_spec.rb
+++ spec/system/borrowing_things_spec.rb
@@ -11,12 +11,9 @@ RSpec.describe "かりもの管理機能", type: :system do
 
     context "Aliceがログインしているとき" do
       before do
-        visit login_path
-        fill_in "Email", with: "alice@example.com"
-        fill_in "Password", with: "password"
-        click_button "Login"
+        login_user("alice@example.com")
       end
-      example "Aliceが作成したかりものが表示される" do
+      example "Aliceが登録したかりものが表示される" do
         expect(page).to have_content "CD"
       end
     end
@@ -24,12 +21,9 @@ RSpec.describe "かりもの管理機能", type: :system do
     context "Bobがログインしているとき" do
       before do
         FactoryBot.create(:user, name: "Bob", email: "bob@example.com")
-        visit login_path
-        fill_in "Email", with: "bob@example.com"
-        fill_in "Password", with: "password"
-        click_button "Login"
+        login_user("bob@example.com")
       end
-      example "Aliceが作成したかりものが表示されない" do
+      example "Aliceが登録したかりものが表示されない" do
         expect(page).to have_no_content "CD"
       end
     end
