113e743b76d9b89619551c25e0d2419e2f8d4f58
masuyama13 <masterxwebm13@gmail.com>
2021-02-23 19:04:40 +0900

    かりもののシステムスペック追加（新規登録・編集・削除）

--- spec/system/borrowing_things_spec.rb
+++ spec/system/borrowing_things_spec.rb
@@ -28,4 +28,68 @@ RSpec.describe "かりもの管理機能", type: :system do
       end
     end
   end
+
+  describe "新規登録する" do
+    before do
+      FactoryBot.create(:user, email: "test@example.com")
+    end
+
+    context "ユーザーがログインしているとき" do
+      example "かりものを新規登録できる" do
+        login_user("test@example.com")
+        visit new_borrowing_thing_path
+        within "form[name=borrowing_thing]" do
+          fill_in "名称", with: "新しいかりもの"
+          fill_in "相手", with: "持ち主"
+        end
+        click_on "登録する"
+        visit borrowing_things_path
+        expect(page).to have_content "新しいかりもの"
+      end
+    end
+  end
+
+  describe "編集する" do
+    before do
+      user = FactoryBot.create(:user, email: "test@example.com")
+      FactoryBot.create(:borrowing_thing, name: "借りた物", user: user)
+    end
+
+    context "ユーザーがログインしているとき" do
+      before do
+        login_user("test@example.com")
+      end
+      example "登録されているかりものを編集する" do
+        visit borrowing_things_path
+        click_on "Edit"
+        within "form[name=borrowing_thing]" do
+          fill_in "名称", with: "借りている物"
+        end
+        click_on "更新する"
+        visit borrowing_things_path
+        expect(page).to have_no_content "借りた物"
+        expect(page).to have_content "借りている物"
+      end
+    end
+  end
+
+  describe "削除する" do
+    before do
+      user = FactoryBot.create(:user, email: "test@example.com")
+      FactoryBot.create(:borrowing_thing, name: "借りた物", user: user)
+    end
+
+    context "ユーザーがログインしているとき" do
+      before do
+        login_user("test@example.com")
+      end
+      example "登録されているかりものを削除する", js: true do
+        visit borrowing_things_path
+        accept_confirm do
+          click_on "Destroy"
+        end
+        expect(page).to have_no_content "借りた物"
+      end
+    end
+  end
 end
