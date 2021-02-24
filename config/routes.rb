2c6be2e5f89608ef7242d123581f337cd8ec548d
masuyama13 <masterxwebm13@gmail.com>
2021-02-24 14:20:57 +0900

    かりもののshowアクションを削除

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   resources :users, except: [:index]
-  resources :borrowing_things
+  resources :borrowing_things, except: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
